ecfafd23cf87b706ec0668a52679906d1bf0938c
Hadhi Hassan m <136085916+hadhihassan@users.noreply.github.com>
2024-06-15 11:34:07 -0700

    Update index.js 1212

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const flash = require("connect-flash");
-const cookieParser = require("cookie-parser");
-const nocache = require("nocache");
-const morgan = require("morgan");
-const path = require("path");
-const Customer = require('./Models/customerModel')
-require("dotenv").config();
-const app = express();
+    const express = require("express");
+    const mongoose = require("mongoose");
+    const session = require("express-session");
+    const flash = require("connect-flash");
+    const cookieParser = require("cookie-parser");
+    const nocache = require("nocache");
+    const path = require("path");
+    const app = express();
 
-app.set("views", path.join(__dirname, "views"));
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
-app.use(flash());
-app.use(nocache());
-app.use(cookieParser());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}));
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-// Routes
-const customerRoute = require("./Route/customerRoute");
-const adminRoute = require("./Route/adminRoute");
-app.use("/", customerRoute);
-app.use("/admin", adminRoute);
+    require("dotenv").config();
 
 
 
-// Database connection
-const url = `mongodb+srv://timezone_admin:timezone_admin@cluster0.a2fqu5o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
+    // Middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ limit: '100mb', extended: true }));
+    app.use(cookieParser());
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    }));
+    app.use(express.static("public"));
 
-let cachedDb = null;
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "ejs");
 
-async function connectToDatabase() {
-    if (cachedDb) {
-        return cachedDb;
+    app.use(flash());
+    app.use(nocache());
+
+    // Routes
+    const customerRoute = require("./Route/customerRoute");
+    const adminRoute = require("./Route/adminRoute");
+    app.use("/", customerRoute);
+    app.use("/admin", adminRoute);
+
+
+
+    // Database connection
+    const url = `mongodb+srv://timezone_admin:timezone_admin@cluster0.a2fqu5o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    let isConnected = false;
+
+    async function connectToDatabase() {
+        if (!isConnected) {
+            try {
+                await mongoose.connect(url, connectionParams);
+                isConnected = true;
+                console.log('Connected to database');
+            } catch (err) {
+                console.error(`Error connecting to the database. \n${err}`);
+            }
+        }
     }
-    console.log('Connecting to database...');
-    try {
-        const db = await mongoose.connect(url, connectionParams);
-        cachedDb = db;
-        console.log('Connected to database');
-        return db;
-    } catch (err) {
-        console.error('Database connection error:', err);
-        throw err;
-    }
-}
+    connectToDatabase()
+    // Start the server
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log("Server is running on port", PORT));
 
-connectToDatabase()
-// Start the server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log("Server is running on port", PORT));
-
-app.use((req, res) => {
-    res.status(404).render("User/404", { message: "" });
-});
+    app.use((req, res) => {
+        res.status(404).render("User/404", { message: "" });
+    });
